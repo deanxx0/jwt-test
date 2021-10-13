a3464b513757e045f57b13d206e7d67c3d4863b3
deanxx <deanxx999@gmail.com>
2021-10-13 18:04:29 +0900

    change api query to param

--- src/training/training.controller.ts
+++ src/training/training.controller.ts
@@ -1,15 +1,11 @@
-import { Body, Controller, Get, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { AugmentationDocument } from 'src/augmentation/augmentation.schema';
+import { ApiResponseDto } from 'src/api-response.dto';
 import { AugmentationService } from 'src/augmentation/augmentation.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { DirectoryDocument } from 'src/directory/directory.schema';
 import { DirectoryService } from 'src/directory/directory.service';
-import { TrainingConfigurationDocument } from 'src/training-configuration/training-configuration.schema';
 import { TrainingConfigurationService } from 'src/training-configuration/training-configuration.service';
-import { CreateTrainingDto } from './create-training.dto';
 import { PostTrainingDto } from './post-training.dto';
-import { TrainingDocument } from './training.schema';
 import { TrainingService } from './training.service';
 
 @Controller('training')
@@ -28,7 +24,7 @@ export class TrainingController {
   @ApiBearerAuth()
   @ApiOperation({ summary: 'create training'})
   @ApiBody({ type: PostTrainingDto })
-  async createTraining(@Body() postTrainingDto: PostTrainingDto) {
+  async createTraining(@Body() postTrainingDto: PostTrainingDto): Promise<ApiResponseDto> {
     console.log(`create training!`);
     const directoryDoc = await this.directoryService.createFromTraining(postTrainingDto);
     const trainingConfigurationDoc = await this.trainingConfigurationService.create(postTrainingDto);
@@ -36,10 +32,7 @@ export class TrainingController {
     const createdTrainingDoc = await this.trainingService.create(
       directoryDoc._id, trainingConfigurationDoc._id, augmentationDoc._id, postTrainingDto.name
     );
-    const success = 
-      directoryDoc && trainingConfigurationDoc && augmentationDoc
-      != null || undefined 
-      ? true : false;
+    const success = createdTrainingDoc != null ? true : false;
     return {
       success: success,
       result: createdTrainingDoc,
@@ -48,13 +41,20 @@ export class TrainingController {
 
   @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
-  @Get('byid')
+  @Get(':_id')
   @ApiBearerAuth()
   @ApiOperation({ summary: 'get training by id'})
-  async findOneById(@Query() query: any): Promise<TrainingDocument> {
+  async findOneById(@Param('_id') _id: string): Promise<ApiResponseDto> {
     console.log(`get training by id!`);
-    return this.trainingService.findOneById(query._id);
+    const findResult = await this.trainingService.findOneById(_id);
+    const success = findResult != null ? true : false;
+    return {
+      success: success,
+      result: findResult,
+    }
+    // 학습 조회
+    // id를 기반으로 학습서버에 요청
+    // 학습서버의 응답을 필요할 경우 정제하여 프론트에 응답
+    // 응답 result dto 따로 필요할듯. 응답 마다 dto가 있어야하는가.
   }
-
-  
 }
