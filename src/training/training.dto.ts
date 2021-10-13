a9922846bcbb79f570a5b10e56dcd6460301a8b7
deanxx <deanxx999@gmail.com>
2021-10-13 10:05:15 +0900

    add id property to training dto

--- src/training/training.dto.ts
+++ src/training/training.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
-import { ObjectId } from "mongoose";
 
 export class TrainingDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
