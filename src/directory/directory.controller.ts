ff3919985035826854a9c0ea7197e0feeccea795
deanxx <deanxx999@gmail.com>
2021-10-14 11:47:26 +0900

    add get directories api

---
+++ src/directory/directory.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiResponseDto } from 'src/api-response.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { DirectoryService } from './directory.service';
+
+@Controller('directories')
+export class DirectoryController {
+  constructor(
+    private dirctoryService: DirectoryService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(ValidationPipe)
+  @Get(':_id')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'get directory' })
+  async getDirectoriesBy_id(@Param('_id') _id: string): Promise<ApiResponseDto> {
+    console.log(`get directories by id!`);
+    const directoryDoc = await this.dirctoryService.findOneById(_id);
+    const success = directoryDoc != null ? true : false;
+    return {
+      success: success,
+      result: directoryDoc,
+    }
+  }
+}
--- src/directory/directory.module.ts
+++ src/directory/directory.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Directory, DirectorySchema } from './directory.schema';
 import { DirectoryService } from './directory.service';
+import { DirectoryController } from './directory.controller';
 
 @Module({
   imports: [
@@ -9,5 +10,6 @@ import { DirectoryService } from './directory.service';
   ],
   providers: [DirectoryService],
   exports: [DirectoryService],
+  controllers: [DirectoryController],
 })
 export class DirectoryModule {}
